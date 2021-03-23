408fd0348f8a4739e0e982c2beaaa04f02153bcc
coding-tristanberris <coding.berris.tristan@gmail.com>
2021-03-22 20:55:01 -0400

    minor changes

--- clientsrc/src/views/Create.vue
+++ clientsrc/src/views/Create.vue
@@ -1,13 +1,24 @@
 <template>
+<div class="row justify-content-center">
+<div class="col-10">
+
+  <form class="">
+  <div class="form-group">
+    <label for="exampleInputEmail1">Name of Recipe:</label>
+    <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter recipe name">
+  </div>
   <form>
   <div class="form-group">
-    <label for="exampleInputEmail1">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+    <label for="exampleFormControlFile1">Image of Recipe</label>
+    <input type="file" class="form-control-file" id="exampleFormControlFile1">
   </div>
+</form>
   <div class="form-group">
-    <label for="exampleInputPassword1">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+    <h1>Steps</h1>
+    <label for="exampleInputPassword1"></label>
+    <input class="form-control form-control-sm" type="text" placeholder="Step 1">
+    <input class="form-control form-control-sm" type="text" placeholder="Step 2">
+    <input class="form-control form-control-sm" type="text" placeholder="Step 3">
   </div>
   <div class="form-check">
     <input type="checkbox" class="form-check-input" id="exampleCheck1">
@@ -15,6 +26,8 @@
   </div>
   <button type="submit" class="btn btn-primary">Submit</button>
 </form>
+</div>
+</div>
 </template>
 
 <script>
