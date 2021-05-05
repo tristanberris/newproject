1d0da680474dd795d32fead3c0e9e9f8108ab2f4
coding-tristanberris <coding.berris.tristan@gmail.com>
2021-05-04 21:56:52 -0400

    home page looking better

--- clientsrc/src/views/Home.vue
+++ clientsrc/src/views/Home.vue
@@ -3,15 +3,19 @@
     <div class="col-12">
       <navbar></navbar>
     </div>
-    <div class="cover-image-container">
-      <img class="cover-image" src="../assets/pasta.jpg" alt />
+    <div class="cover-image-container col-12">
+      <!-- <img class="cover-image" src="../assets/pasta.jpg" alt /> -->
       <div class="centered col-12">No Bloat</div>
       <div class="centered-lower col-12">Only Recipes</div>
-
-      <div class="row">
-      <a class="gs-button btn " role="button" href="/#/recipes">get started</a>
-      <p class="middle-or ">OR</p>
-      <a class="cyo-button btn " role="button" href="/#/create">create your own</a>
+      <div class="col-12 spacing"></div>
+      <div class="col-12 col-sm-4 no-block">
+        <a class="gs-button btn" role="button" href="/#/recipes">get started</a>
+      </div>
+      <div class="col-12 col-sm-1 no-block">
+        <span class="middle-or">OR</span>
+      </div>
+      <div class="col-12 col-sm-4 no-block">
+        <a class="cyo-button btn" role="button" href="/#/create">create your own</a>
       </div>
     </div>
   </div>
@@ -35,6 +39,9 @@ export default {
 </script>
 <style scoped>
 /* #212529 */
+a{
+  width: 100%;
+}
 .left-side {
   background-color: #2a2e32;
   color: white;
@@ -53,37 +60,51 @@ export default {
   padding: 0;
 }
 .cover-image {
-  object-fit: cover;
+  /* object-fit: cover;
   width: 100%;
   min-height: 800px;
-  opacity: 0.4;
+  opacity: 0.4; */
 }
 .cover-image-container {
   position: relative;
   text-align: center;
   color: white;
+
+  height: 100vh;
+}
+.cover-image-container::before {
+  content: "";
+  background-image: url(https://i.imgur.com/ZStG4IE.jpg);
+  background-size: cover;
+  position: absolute;
+  text-align: center;
+  top: 0px;
+  right: 0px;
+  bottom: 0px;
+  left: 0px;
+  opacity: 0.4;
 }
 .centered {
-  position: absolute;
+  /* position: absolute;
   top: 20%;
-  left: 50%;
-  transform: translate(-50%, -50%);
+  left: 50%; */
+  /* transform: translate(-50%, -50%); */
   font-size: 3.5em;
   /* color: #f4403b; */
 }
 .centered-lower {
-  position: absolute;
+  /* position: absolute;
   top: 40%;
-  left: 50%;
-  transform: translate(-50%, -50%);
+  left: 50%; */
+  /* transform: translate(-50%, -50%); */
   font-size: 3.5em;
   /* color: #f4403b; */
 }
 .gs-button {
-  position: absolute;
+  /* position: absolute;
   top: 70%;
   left: 50%;
-  transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%); */
   background-color: #f4403b;
   color: white;
   padding: 10px;
@@ -92,10 +113,10 @@ export default {
   font-size: 2em;
 }
 .cyo-button {
-  position: absolute;
+  /* position: absolute;
   top: 90%;
   left: 50%;
-  transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%); */
   background-color: #f4403b;
   color: white;
   padding: 10px;
@@ -103,14 +124,19 @@ export default {
   font-weight: bold;
   font-size: 2em;
 }
-.middle-or{
-  position: absolute;
+.middle-or {
+  /* position: absolute;
   top: 80%;
   left: 50%;
-  transform: translate(-50%, -50%);
+  transform: translate(-50%, -50%); */
   color: white;
-    font-weight: lighter;
+  font-weight: lighter;
   font-size: 1.5em;
-
+}
+.no-block{
+  display: inline-block;
+}
+.spacing{
+  height: 35vh;
 }
 </style>
